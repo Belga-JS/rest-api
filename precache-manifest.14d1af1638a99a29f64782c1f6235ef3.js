6e78dd47584cee7e7b70588287f329a0e0eb829d
Belga-JS <Meherzi.belga@yahoo.com>
2020-01-02 10:20:44 +0100

    Updates

---
+++ precache-manifest.14d1af1638a99a29f64782c1f6235ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1b3a19b2465ae8a5d8c4b1d3420ab6",
+    "url": "/rest-api/index.html"
+  },
+  {
+    "revision": "2fd4845ba894e461c746",
+    "url": "/rest-api/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9621e9f761d1eabe9b4c",
+    "url": "/rest-api/static/js/2.8982dcdc.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/rest-api/static/js/2.8982dcdc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fd4845ba894e461c746",
+    "url": "/rest-api/static/js/main.eb76ef14.chunk.js"
+  },
+  {
+    "revision": "cfe5b390b57c4d2ceec3",
+    "url": "/rest-api/static/js/runtime-main.524a14b0.js"
+  }
+]);
